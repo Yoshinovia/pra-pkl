b59191c4ac1bdb233a99fc95229cd922d3606f14
Elaina <ywdhelaina@gmail.com>
2026-07-21 13:39:26 +0700

    create middleware1

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,23 +1,17 @@
-// src/middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  // 1. Ambil cookie token
   const token = request.cookies.get('token')?.value;
-
-  // 2. Ambil path URL yang sedang diakses
   const { pathname } = request.nextUrl;
 
-  // 3. Tentukan rute mana saja yang butuh proteksi (Protected Routes)
+//   route cuek
   const isProtectedRoute = pathname.startsWith('/dashboard') || pathname.startsWith('/reports');
 
-  // 4. Jika mencoba akses rute terproteksi TAPI tidak punya token -> Redirect ke Login
   if (isProtectedRoute && !token) {
-    return NextResponse.redirect(new URL('/', request.url)); // Kembali ke halaman utama/login
+    return NextResponse.redirect(new URL('/', request.url)); 
   }
 
-  // 5. Jika sudah login tapi mencoba akses halaman login lagi -> Redirect ke Dashboard
   if (pathname === '/' && token) {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
@@ -25,7 +19,7 @@ export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-// Konfigurasimatcher agar middleware hanya berjalan di rute tertentu (menghemat performa)
+
 export const config = {
   matcher: ['/', '/dashboard/:path*', '/reports/:path*'],
 };
